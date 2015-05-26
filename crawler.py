7d8aa3b0f3d2814c999abb2887dbc78424de2507
Michiganx <michigan@ukr.net>
2015-05-26 16:43:02 +0300

    everything

--- crawler.py
+++ crawler.py
@@ -49,7 +49,7 @@ for order in orders:
         node_label, rel_label = ORDERS_FOREIGN_KEYS[prop]
         fk_value = order[prop]
         extern_node = con.find_one(node_label, 'id' + node_label + 's', fk_value)
-        con.create(Relationship(extern_node, rel_label, order))
+        con.create(Relationship(extern_node, rel_label, order)) 
 
 experts = con.find("Expert", limit=1)
 for expert in experts:
